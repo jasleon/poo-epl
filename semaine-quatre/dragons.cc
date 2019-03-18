7b7b956eefc0ffa476cf5180f0c8306c1dd7e861
Antonio Sanchez <antonio.buap@gmail.com>
2019-03-18 10:43:23 -0600

    Add declaration of class Creature

--- semaine-quatre/dragons.cc
+++ semaine-quatre/dragons.cc
@@ -13,94 +13,161 @@ class Creature
   /*****************************************************
    * Compléter le code à partir d'ici
    *****************************************************/
+    protected:
+        const string nom_;
+        int niveau_;
+        int points_de_vie_;
+        int force_;
+        int position_;
+    
+    public:
 
-         << ", niveau: "
-         << ", points de vie: "
-         << ", force: "
-         << ", points d'attaque: "
-         << ", position: "
+        /**
+         * Class constructor
+         */
+        Creature(string name, int level, int health_points, int force, int position = 0)
+            : nom_(name), niveau_(level), points_de_vie_(health_points), force_(force), position_(position) {}
 
-         << " n'est plus !"
+        /**
+         * A method returning true if the creature is alive
+         */
+        bool vivant() const
+        {
+            return (points_de_vie_ > 0);
+        }
 
+        /**
+         * A method returning the number of attack points that can be inflicted by the creature to others
+         */ 
+        int points_attaque() const
+        {
+            return (vivant() ? (niveau_ * force_) : 0);
+        }
+
+        /**
+         * A method to add an integer to the position of the creature
+         */ 
+        void deplacer(int valeur) 
+        { 
+            position_ += valeur; 
+        }
+
+        /**
+         * A method to say goodbye
+         */ 
+        void adieux() const
+        {
+            cout << nom_ << " n'est plus !" << endl;
+        }
+
+        /**
+         * A method to substract an integer from the number of health points of the creature (if alive)
+         * If the creature dies, its number of health points is set to zero and the method adieux is called
+         */ 
+        void faiblir(int valeur)
+        {
+            if(vivant())
+            {
+                points_de_vie_ -= valeur;
+                if(!vivant())
+                {
+                    points_de_vie_ = 0;
+                    adieux();
+                }
+            }
+        }
+
+        /**
+         * A method to display information about the creature
+         */ 
+        void afficher() const
+        {
+            cout                    << nom_             <<  
+            ", niveau: "            << niveau_          << 
+            ", points de vie: "     << points_de_vie_   << 
+            ", force: "             << force_           << 
+            ", points d'attaque: "  << points_attaque() << 
+            ", position: "          << position_        << endl;
+        }
 };
 /*******************************************
  * Ne rien modifier après cette ligne.
  *******************************************/
 int main()
 {
-  Dragon dragon("Dragon rouge"   , 2, 10, 3, 20         );
-  Hydre  hydre ("Hydre maléfique", 2, 10, 1, 10, 1,  42 );
+//   Dragon dragon("Dragon rouge"   , 2, 10, 3, 20         );
+//   Hydre  hydre ("Hydre maléfique", 2, 10, 1, 10, 1,  42 );
 
-  dragon.afficher();
-  cout << "se prépare au combat avec :" << endl;
-  hydre.afficher();
+//   dragon.afficher();
+//   cout << "se prépare au combat avec :" << endl;
+//   hydre.afficher();
 
-  cout << endl;
-  cout << "1er combat :" << endl;
-  cout << "    les créatures ne sont pas à portée, donc ne peuvent pas s'attaquer."
-       << endl;
-  combat(dragon, hydre);
+//   cout << endl;
+//   cout << "1er combat :" << endl;
+//   cout << "    les créatures ne sont pas à portée, donc ne peuvent pas s'attaquer."
+//        << endl;
+//   combat(dragon, hydre);
 
-  cout << "Après le combat :" << endl;
-  dragon.afficher();
-  hydre.afficher();
+//   cout << "Après le combat :" << endl;
+//   dragon.afficher();
+//   hydre.afficher();
 
-  cout << endl;
-  cout << "Le dragon vole à proximité de l'hydre :" << endl;
-  dragon.voler(hydre.position() - 1);
-  dragon.afficher();
+//   cout << endl;
+//   cout << "Le dragon vole à proximité de l'hydre :" << endl;
+//   dragon.voler(hydre.position() - 1);
+//   dragon.afficher();
 
-  cout << endl;
-  cout << "L'hydre recule d'un pas :" << endl;
-  hydre.deplacer(1);
-  hydre.afficher();
+//   cout << endl;
+//   cout << "L'hydre recule d'un pas :" << endl;
+//   hydre.deplacer(1);
+//   hydre.afficher();
 
-  cout << endl;
-  cout << "2e combat :" << endl;
-  cout << "\
-  + l'hydre inflige au dragon une attaque de 3 points\n\
-      [ niveau (2) * force (1) + poison (1) = 3 ] ;\n\
-  + le dragon inflige à l'hydre une attaque de 6 points\n\
-      [ niveau (2) * force (3) = 6 ] ;\n\
-  + pendant son attaque, le dragon perd 2 points de vie supplémentaires\n\
-       [ correspondant à la distance entre le dragon et l'hydre : 43 - 41 = 2 ].\
-" << endl;
-  combat(dragon, hydre);
+//   cout << endl;
+//   cout << "2e combat :" << endl;
+//   cout << "\
+//   + l'hydre inflige au dragon une attaque de 3 points\n\
+//       [ niveau (2) * force (1) + poison (1) = 3 ] ;\n\
+//   + le dragon inflige à l'hydre une attaque de 6 points\n\
+//       [ niveau (2) * force (3) = 6 ] ;\n\
+//   + pendant son attaque, le dragon perd 2 points de vie supplémentaires\n\
+//        [ correspondant à la distance entre le dragon et l'hydre : 43 - 41 = 2 ].\
+// " << endl;
+//   combat(dragon, hydre);
 
-  cout << "Après le combat :" << endl;
-  dragon.afficher();
-  hydre.afficher();
+//   cout << "Après le combat :" << endl;
+//   dragon.afficher();
+//   hydre.afficher();
 
-  cout << endl;
-  cout << "Le dragon avance d'un pas :" << endl;
-  dragon.deplacer(1);
-  dragon.afficher();
+//   cout << endl;
+//   cout << "Le dragon avance d'un pas :" << endl;
+//   dragon.deplacer(1);
+//   dragon.afficher();
 
-  cout << endl;
-  cout << "3e combat :" << endl;
-  cout << "\
-  + l'hydre inflige au dragon une attaque de 3 points\n\
-      [ niveau (2) * force (1) + poison (1) = 3 ] ;\n\
-  + le dragon inflige à l'hydre une attaque de 6 points\n\
-      [ niveau (2) * force (3) = 6 ] ;\n\
-  + pendant son attaque, le dragon perd 1 point de vie supplémentaire\n\
-       [ correspondant à la distance entre le dragon et l'hydre : 43 - 42 = 1 ] ;\n\
-  + l'hydre est vaincue et le dragon monte au niveau 3.\
-" << endl;
-  combat(dragon, hydre);
+//   cout << endl;
+//   cout << "3e combat :" << endl;
+//   cout << "\
+//   + l'hydre inflige au dragon une attaque de 3 points\n\
+//       [ niveau (2) * force (1) + poison (1) = 3 ] ;\n\
+//   + le dragon inflige à l'hydre une attaque de 6 points\n\
+//       [ niveau (2) * force (3) = 6 ] ;\n\
+//   + pendant son attaque, le dragon perd 1 point de vie supplémentaire\n\
+//        [ correspondant à la distance entre le dragon et l'hydre : 43 - 42 = 1 ] ;\n\
+//   + l'hydre est vaincue et le dragon monte au niveau 3.\
+// " << endl;
+//   combat(dragon, hydre);
 
-  cout << "Après le combat :" << endl;
-  dragon.afficher();
-  hydre.afficher();
+//   cout << "Après le combat :" << endl;
+//   dragon.afficher();
+//   hydre.afficher();
 
-  cout << endl;
-  cout << "4e Combat :" << endl;
-  cout << "    quand une créature est vaincue, rien ne se passe." << endl;
-  combat(dragon, hydre);
+//   cout << endl;
+//   cout << "4e Combat :" << endl;
+//   cout << "    quand une créature est vaincue, rien ne se passe." << endl;
+//   combat(dragon, hydre);
 
-  cout << "Après le combat :" << endl;
-  dragon.afficher();
-  hydre.afficher();
+//   cout << "Après le combat :" << endl;
+//   dragon.afficher();
+//   hydre.afficher();
 
   return 0;
 }
